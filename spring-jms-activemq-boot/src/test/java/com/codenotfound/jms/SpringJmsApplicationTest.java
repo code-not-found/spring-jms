ba08a9e5ac2a2b29b3e9ba26493e34d1cf02ede4
codenotfound.com <codenotfound.com@gmail.com>
2017-05-02 17:51:14 +0200

    Add boot project

---
+++ spring-jms-activemq-boot/src/test/java/com/codenotfound/jms/SpringJmsApplicationTest.java
@@ -0,0 +1,33 @@
+package com.codenotfound.jms;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.codenotfound.jms.consumer.Receiver;
+import com.codenotfound.jms.producer.Sender;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJmsApplicationTest {
+
+  @Autowired
+  private Sender sender;
+
+  @Autowired
+  private Receiver receiver;
+
+  @Test
+  public void testReceive() throws Exception {
+    sender.send("helloworld.q", "Hello Spring JMS ActiveMQ!");
+
+    receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+  }
+}
